21d24c1878f2d1de12429aa640f1342bc373c5ac
Enyabuto <elishahyperx@yahoo.com>
2020-12-02 21:06:25 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState ([
+    {
+      username: "elisha_on",
+      caption: "WOW it works",
+      imageUrl: "https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png"
+    },
+    {
+      username: "joabn30",
+      caption: "Damn! Those are beautiful",
+      imageUrl: "https://asset.bloomnation.com/c_pad,d_vendor:global:catalog:product:image.png,f_auto,fl_preserve_transparency,q_auto/v1605352255/vendor/2183/catalog/product/2/0/20200424092013_file_5ea3580d59716_5ea35828e648b.jpg"
+    },
+    {
+      username: "NBAonTNT",
+      caption: "LBJ is on fire",
+      imageUrl: "https://pyxis.nymag.com/v1/imgs/847/0f7/504c63a03d8a751a5cbeda0bc064306bb4-lebron-james.rsquare.w1200.jpg"
+    },
+  ]);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_header">
+        <img className="app_headerImage" 
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+          alt=""
+        />
+      </div>
+
+      {
+        posts.map(post => (
+            <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+          ))
+      };
+      
+      
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+//import firebase from 'firebase';
+//intialize database
+
+
+//const firebaseConfig = {
+    //apiKey: "AIzaSyAjakaRg9gXtX_NKMGNmRmfgWvMLRA9UHk",
+    //authDomain: "instagram-clone-react-elisha.firebaseapp.com",
+    //projectId: "instagram-clone-react-elisha",
+    //storageBucket: "instagram-clone-react-elisha.appspot.com",
+    //messagingSenderId: "347965510643",
+    //appId: "1:347965510643:web:e1e8a7b1301a0a4d77b886",
+    //measurementId: "G-9XGGV3KR81"
+  //};
+
+  //firebase.initializeApp(config);
+  //export default firebase;
